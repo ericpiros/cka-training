9f986165584cdf64b35c04624d52230d195a8905
Eric John Piros <ericpiros@yahoo.com>
2023-06-02 14:22:52 +0800

    added additional cp node

--- vagrant/ha_cluster_libvert/k8s_req.sh
+++ vagrant/ha_cluster_libvert/k8s_req.sh
@@ -23,6 +23,8 @@ EOF
 sudo sysctl --system
 
 echo "Installing containerd"
+# Do not do this in production.
+echo "deb [trusted=yes] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update && sudo apt install -y containerd
 
 sudo mkdir -p /etc/containerd
@@ -37,8 +39,6 @@ sudo mkdir -p /etc/apt/keyrings
 # Seems to be broken at this time.
 #sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-# Do not do this in production.
-echo "deb [trusted=yes] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 #echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
